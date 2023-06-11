23de41771cd4ffb1b9510d25d3fb342254209002
Pavel Poryvai <42907367+Pavel377dq@users.noreply.github.com>
2023-06-11 04:38:34 +0300

    todoOnHooks

--- src/components/todoApp/todoApp.js
+++ src/components/todoApp/todoApp.js
@@ -209,7 +209,7 @@ export default function App() {
     return (
         <section className="todoapp">
             <header className="header">
-                <h1>todos</h1>
+                <h1>todos on Hooks</h1>
                 <NewTaskForm onAddTask={addTask} />
             </header>
             <section className="main">
